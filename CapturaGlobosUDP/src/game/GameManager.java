237cb6ed456730927d2f1ea279d32dd966eebca5
Maribel Ceballos <maribel.ceballosc@autonoma.edu.co>
2026-04-01 19:30:07 -0500

    Implementado GameManager Singleton con lógica de globos y tiempo

--- CapturaGlobosUDP/src/game/GameManager.java
+++ CapturaGlobosUDP/src/game/GameManager.java
@@ -8,6 +8,90 @@ package game;
  *
  * @author marib
  */
+import model.*;
+import util.RandomUtil;
+
+import java.awt.Color;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GameManager {
-    
+
+    private static GameManager instance;
+
+    private EstadoJuego estado;
+    private Timer timerGlobos;
+    private Timer timerTiempo;
+
+    private GameManager() {
+        estado = new EstadoJuego();
+    }
+
+    public static GameManager getInstance() {
+        if (instance == null) {
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    public void iniciarJuego(Jugador local, Jugador remoto) {
+        estado.setJugadorLocal(local);
+        estado.setJugadorRemoto(remoto);
+
+        iniciarGeneracionGlobos();
+        iniciarTiempo();
+    }
+
+    private void iniciarGeneracionGlobos() {
+        timerGlobos = new Timer();
+        timerGlobos.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                crearGlobo();
+            }
+        }, 0, 2000);
+    }
+
+    private void iniciarTiempo() {
+        timerTiempo = new Timer();
+        timerTiempo.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                int t = estado.getTiempoRestante();
+                estado.setTiempoRestante(t - 1);
+
+                if (t <= 0) {
+                    finalizarJuego();
+                    timerTiempo.cancel();
+                    timerGlobos.cancel();
+                }
+            }
+        }, 1000, 1000);
+    }
+
+    private void crearGlobo() {
+        int x = RandomUtil.randomX();
+        int y = RandomUtil.randomY();
+
+        Globo g = new Globo(x, y, 30, Color.RED);
+        estado.getGlobos().add(g);
+    }
+
+    public void procesarClick(int x, int y) {
+        for (Globo g : estado.getGlobos()) {
+            if (g.contiene(x, y)) {
+                estado.getGlobos().remove(g);
+                estado.getJugadorLocal().sumarPunto();
+                break;
+            }
+        }
+    }
+
+    private void finalizarJuego() {
+        System.out.println("Juego terminado");
+    }
+
+    public EstadoJuego getEstado() {
+        return estado;
+    }
 }
--- CapturaGlobosUDP/src/util/RandomUtil.java
+++ CapturaGlobosUDP/src/util/RandomUtil.java
@@ -8,6 +8,17 @@ package util;
  *
  * @author marib
  */
+import java.util.Random;
+
 public class RandomUtil {
-    
+
+    private static Random rand = new Random();
+
+    public static int randomX() {
+        return rand.nextInt(600) + 50;
+    }
+
+    public static int randomY() {
+        return rand.nextInt(400) + 50;
+    }
 }
